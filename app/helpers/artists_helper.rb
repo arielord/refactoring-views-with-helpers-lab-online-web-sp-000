a74f75d5e6a9095b7aa4afe86fedcceb098215a2
Ariel <arielord@buffalo.edu>
2020-04-27 21:43:14 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,5 +1,9 @@
 module ArtistsHelper
   def display_artist(artist)
+    if artist.is_a?(Song)
+      artist = Song.artist
+    end
+    
     if artist.songs.size == 0
       link_to artist.name, artist_path(artist)
     end
